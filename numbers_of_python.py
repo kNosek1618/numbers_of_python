473c1aa3f906003caf1236e34772e8d5d82c463d
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-05 01:47:26 +0200

    variables into numbers 1.1

--- numbers_of_python.py
+++ numbers_of_python.py
@@ -15,7 +15,7 @@ print (math.sqrt(16))
 #we can add letters if we convert number to string
 #{
 print (str(math.pi) + " is Pi")
-print (str(math.e) + " is magic e")
+print (str(math.e) + " is \'magic\' e")
 #}
 
 print ("-----------------")
@@ -23,4 +23,14 @@ print ("-----------------")
 #variable into numbers
 my_num1 = 5
 my_num2 = 7
+my_num3 = -4
 print (my_num1 + my_num2)
+print (abs(my_num3))
+print (pow(my_num1,my_num2))
+print (math.sqrt(my_num1))
+print(max(my_num1,my_num2,my_num3))
+print(min(my_num1,my_num2,my_num3))
+print (round(my_num2/my_num1))
+from math import *
+print (floor(3.7))
+print (ceil(3.2))
