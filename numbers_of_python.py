d7f83f65d5609916197d2ac4de42aba64dc1f618
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-05 01:01:23 +0200

    float and integer

---
+++ numbers_of_python.py
@@ -0,0 +1,15 @@
+
+#integer type numbers
+print (2+2)
+print (2 * 3)
+print (pow(2,4))
+print (10 % 3)
+
+print ("")
+
+#float type numbers
+print (4/2)
+import math
+print (math.sqrt(16))
+print (math.pi)
+print (math.e)
